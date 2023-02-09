88416ce15cea3f46ec628720254d8f5773d9be75
Hey.Its_RJ <rishab1300@gmail.com>
2023-02-09 18:00:15 -0500

    Testcases for Nextrad

--- main.py
+++
@@ -1,6 +0,0 @@
-import awscloud.s3.main as aw
-
-
-print(aw.get_all_geos_file_name_by_filter('ABI-L1b-RadC', 2023, '029', '01'))
-
-print(aw.get_geos_aws_link('ABI-L1b-RadC', 2023, '029', '01', 'OR_ABI-L1b-RadC-M6C01_G18_s20230290131183_e20230290133559_c20230290134003.nc'))
--- nexrad_main.py
+++
@@ -1,7 +0,0 @@
-from awscloud.s3 import nexrad_main as awr
-
-print(awr.get_all_nexrad_file_name_by_filter('2023', '02', '04', 'KABR'))
-
-print(awr.get_nexrad_aws_link_by_filename('KABR20230204_000536_V06'))
-
-print(awr.get_nexrad_aws_link('2023', '02', '04', 'KABR', 'KABR20230204_000536_V06'))
